51c8cbab5265937b9166dc1db840342cc4031a9a
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-05-27 21:56:03 -0500

    adding questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,31 @@ $(document).ready(function() {
 
 
 });
+
+
+var triviaQuestions = ["What year was the National Basketball Association first established?", "What team has won the most NBA titles?", "How did Los Angeles Lakers' superstar Kobe Bryant get his name?", "Michael Jordan was drafted third overall in 1984. Which two players were selected ahead of him?", "Which team lost the most games during the 2003-2004 season?", "How many games are in an NBA season?", "How many teams are in the NBA?", "Whose silhouette serves as the modern day logo of the NBA?", "Who holds the Spurs record for most points in a game?", "Who is the Spurs' all-time leader in assists?"];
+
+var answerChoices = [
+  ["1935", "1945", "1946", "1958"],
+  ["The Chicago Bulls", "The Los Angeles Lakers", "The Boston Celtics", "The Philadelphia 76ers"],
+  ["He's named after his Uncle Kobe", "He's named after a type of steak", "He's named after the doctor who delivered him.", "He's named after a city in Southern California where his parents met"],
+  ["Hakeem Olajuwon & Sam Bowie", "Patrick Ewing & Hakeem Olajuwon", "Charles Barkley & Hakeem Olajuwon", "Karl Malone & Sam Bowie"],
+  ["The Orlando Magic", "The LA Clippers", "The Chicago Bulls", "The Golden State Warriors"],
+  ["82", "84", "86", "88"],
+  ["30", "40", "25", "35"],
+  ["Michael Jordan", "Bill Cousy", "Jerry West", "Pat Riley"],
+  ["George Gervin", "David Robinson", "Tim Duncan", "Terry Cummings"],
+  ["Tony Parker", "Johnny Moore", "Alvin Robertson","Avery Johnson"]
+];
+
+var correctAnswer = [
+  "The NBA got its start as the Basketball Association of America in 1946. The league formally got its NBA moniker in 1950.",
+  "The Boston Celtics have won 16 NBA titles including eight in a row from 1959-66.",
+  "Kobe was named after a type of steak (Kobe) seen by his parents on a menu before he was born.",
+  "Hakeem Olajuwon was drafted first overall by the Houston Rockets out of the University of Houston in 1984 and went onto to win an NBA MVP award and back-to-back championships with the Rockets in 1994 and 1995. Sam Bowie was drafted number two overall by the Portland Trail Blazers out of Kentucky.",
+  "The Orlando Magic sucked big-time in 2003-04 losing 61 games and winning just 21.",
+  "There are 82 games in an NBA season",
+  "There are 30 teams in the NBA",
+  "Bill Cousy",
+  "Tony Parker"
+];
