feb2340e1b1ae56be4093eb5fb1034333baed752
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-05-28 03:02:52 -0500

    game still has a few bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,8 +7,10 @@ var clockInterval;
 var gameTime = 30;
 var gameStart;
 var clickSelect;
+
 //Array of Questions
 var triviaQuestions = ["What year was the National Basketball Association first established?", "What team has won the most NBA titles?", "How did Los Angeles Lakers' superstar Kobe Bryant get his name?", "Michael Jordan was drafted third overall in 1984. Which two players were selected ahead of him?", "Which team lost the most games during the 2003-2004 season?", "How many games are in an NBA season?", "How many teams are in the NBA?", "Whose silhouette serves as the modern day logo of the NBA?", "Who holds the Spurs record for most points in a game?", "Who is the Spurs' all-time leader in assists?"];
+
 //Array of Possible Answer Choices
 var answerChoices = [
   ["1935", "1945", "1946", "1958"],
@@ -22,6 +24,7 @@ var answerChoices = [
   ["George Gervin", "David Robinson", "Tim Duncan", "Terry Cummings"],
   ["Tony Parker", "Johnny Moore", "Alvin Robertson","Avery Johnson"]
 ];
+
 //Array of the correct answers
 var correctAnswer = [
   "1946",
@@ -36,10 +39,7 @@ var correctAnswer = [
   "Tony Parker"
 ];
 
-
-
 function displayGame() {
-  timer();
   $("#question").html("<h2>" + triviaQuestions[qCount] + "</h2>");
   $("#choices").html("<p class='choice'>" + answerChoices[qCount][0] + "</p><p class='choice'>" + answerChoices[qCount][1] + "</p><p class='choice'>" + answerChoices[qCount][2] + "</p><p class='choice'>" + answerChoices[qCount][3] + "</p>");
 }
@@ -47,15 +47,14 @@ function displayGame() {
 function timer() {
 	var i = 30;
   clockInterval = setInterval(function() {
-    $("#time").html("<h3>Seconds left: " + i + "</h3>");
+    $("#countDown-timer").html("<h3>Seconds left: " + i + "</h3>");
     if (i === 0) {
       clearInterval();
       timeoutLoss();
-      notAnswered++;
-      $("#time").empty();
-      qCount++;
+      $("#countDown-timer").empty();
+      //qCount++;
     }
-    else {
+    if (i > 0) {
       i--;
     }
   }, 1000);
@@ -64,60 +63,69 @@ function timer() {
 //If player runs out of time, increment not answered count and display correct answer
 function timeoutLoss() {
 	notAnswered++;
-	$("#question").html("<p>Time Left:" + gameTime + "</p>" + "<p>Time's Up! Correct Answer: " + correctAnswer[qCount] + "</p>");
-	setTimeout(wait, 3000);
+	$("#question").html("<h2>Time's Up! Correct Answer: " + correctAnswer[qCount] + "</h2>");
+	setTimeout(advance, 2500);
 }
 
-  /*function advanceNext() {
-    clearInterval(clockInterval);
-    ("#time").empty();
-    if (qCount < 10) {
-      displayGame();
-      timer();
+//Function will either advance to the next question or display the trivia game results
+function advance() {
+	if (qCount < 9) {
+	qCount++;
+	gameTime = 30;
+  displayGame();
+  timer();
+	}
+	else {
+		results();
+	}
+}
 
-    }
-  }
-*/
+function results() {
+  $(".game-area").html("<h2 class='results'>Quiz Completed!" + "</h2>" + "<p>Correct Answers: " + numCorrect + "</p>" + "<p>Wrong Answers: " + numIncorrect + "</p>" + "<p>Not Answered: " + notAnswered + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>");
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+function reset() {
+	qCount = 0;
+	numCorrect = 0;
+	numIncorrect = 0;
+	notAnswered = 0;
+	gameTime = 30;
+	displayGame();
+  timer();
+}
 
 $(document).ready(function() {
   function startButton() {
     $("#start").html("<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>");
   }
   startButton();
+
   //Once button is clicked game begins and creates html content
   $("body").on("click", ".start-button", function(event){
 	   displayGame();
-
+     timer();
      $("#start").empty();
   });
 
+  //Resets the game
+  $("body").on("click", ".reset-button", function(event){
+  	resetGame();
+  });
+
+  //Compares the string of the selected Answer with the string of the correct answer to determine whether correct or incorrect
   $("body").on("click", ".choice", function(event){
   	clickSelect = $(this).text();
   	if(clickSelect === correctAnswer[qCount]) {
-  		alert("correct");clearInterval(clockInterval);
-  		//generateWin();
+  		clearInterval(clockInterval);
+      numCorrect++;
+      $("#question").html("<h2>Correct! The answer is: " + correctAnswer[qCount] + "</h2>");
+    	setTimeout(advance, 2500);
   	}
   	else {
-  		alert("wrong answer!");
   		clearInterval(clockInterval);
-  		//generateLoss();
+      numIncorrect++;
+      $("#question").html("<h2>Wrong! The answer is: " + correctAnswer[qCount] + "</h2>");
+    	setTimeout(advance, 2500);
   	}
   });
-
-
-
-
 });
